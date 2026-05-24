2d9cad5503f3c0ce73d77c09f9ef999aaf51108e
Gokulgokulbasu1843-art <gokubasu1843@gmail.com>
2026-05-24 21:09:24 +0530

    new neochat upload

--- src/server/server.cjs
+++ src/server/server.cjs
@@ -1,4 +1,6 @@
-require("../db.cjs");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
 
 const express = require("express");
 const http = require("http");
@@ -6,6 +8,15 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected 🚀");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(cors());
 
